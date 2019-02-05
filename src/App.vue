f847e9b0c9d71f3a637a9184015a099a3b353e33
Bastian <bastianlc@live.cl>
2019-02-05 02:45:08 -0300

    Fix URL deploy build

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #app
-  img(src='https://bastianvargas.github.io/musicpage/dist/logo.png')
+  img(src='dist/logo.png')
   h1 Bastian Music
   select(v-model="selectedCountry")
     option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
