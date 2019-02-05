6bb995fe616d56f2c087518ce110025a359e8a9c
Bastian <bastianlc@live.cl>
2019-02-05 02:33:02 -0300

    Fix error URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #app
-  img(src='./assets/logo.png')
+  img(src='https://bastianvargas.github.io/musicpage/dist/logo.png')
   h1 Bastian Music
   select(v-model="selectedCountry")
     option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
